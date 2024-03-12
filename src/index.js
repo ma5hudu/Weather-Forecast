c90c8e9268697a950d979fd50277c1fadc9ba964
Mashudu Mphaphuli <mphaphulimashudu546@gmail.com>
2024-03-12 12:35:51 +0200

    Change city value based on the search input

--- src/index.js
+++ src/index.js
@@ -26,6 +26,19 @@ function dateTime() {
 
   return `${day} ${hour}:${minutes}`;
 }
+
 //update the time in the html file
 let todaysDate = document.querySelector(".current-date");
 todaysDate.innerHTML = dateTime();
+
+
+//update the name of the city with the searched city value.
+function findCity(event) {
+  event.preventDefault();
+  let cityValue = document.querySelector(".serach-value");
+  let city = document.querySelector(".current-city");
+  city.innerHTML = `${cityValue.value}`;
+}
+
+let searchCity = document.querySelector(".search-button");
+searchCity.addEventListener("click", findCity);
